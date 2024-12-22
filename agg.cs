2ea9542d07d74425d660461131b0aea15ead05c9
956784 <146649071+956784@users.noreply.github.com>
2024-12-22 16:16:49 +0300

    Update agg.cs

--- agg.cs
+++ agg.cs
@@ -1,25 +1,43 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.7.34024.191
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "agg", "agg\agg.csproj", "{B014DEF5-0EA0-43B9-B086-BAB6195DF6C8}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|Any CPU = Debug|Any CPU
-		Release|Any CPU = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{B014DEF5-0EA0-43B9-B086-BAB6195DF6C8}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
-		{B014DEF5-0EA0-43B9-B086-BAB6195DF6C8}.Debug|Any CPU.Build.0 = Debug|Any CPU
-		{B014DEF5-0EA0-43B9-B086-BAB6195DF6C8}.Release|Any CPU.ActiveCfg = Release|Any CPU
-		{B014DEF5-0EA0-43B9-B086-BAB6195DF6C8}.Release|Any CPU.Build.0 = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {AAE51A1C-227D-4B86-A67C-146D97E2B261}
-	EndGlobalSection
-EndGlobal
+using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем и настраиваем потоки
+        Thread chickenThread = new Thread(PrintChicken);
+        Thread eggThread = new Thread(PrintEgg);
+
+        // Запускаем потоки
+        chickenThread.Start();
+        eggThread.Start();
+
+        // Ждем завершения потоков
+        chickenThread.Join();
+        eggThread.Join();
+
+        // Определяем, какой поток завершился последним
+        // Если chickenThread все еще активен, значит eggThread завершился первым
+        if (chickenThread.IsAlive)
+        {
+            Console.WriteLine("Последним завершилось: Яйцо");
+        }
+        else
+        {
+            Console.WriteLine("Последним завершилось: Курица");
+        }
+    }
+
+    // Метод, который выполняется в потоке, выводит "Курица"
+    static void PrintChicken()
+    {
+        Console.WriteLine("Курица");
+    }
+
+    // Метод, который выполняется в потоке, выводит "Яйцо"
+    static void PrintEgg()
+    {
+        Console.WriteLine("Яйцо");
+    }
+}
